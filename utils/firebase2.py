ea8975d69e239c5754fe5c2dda857d4c3fd48d69
Vinzzztty <kevinarnandes21@gmail.com>
2024-06-12 00:44:44 +0700

    MAJOR UPDATE BRANCH DEVELOPMENT

--- utils/firebase2.py
+++ utils/firebase2.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def get_relayph_from_firebase(url):
     """
     Retrieves the 'relayPH' value from Firebase.
@@ -13,15 +14,18 @@ def get_relayph_from_firebase(url):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        relayph_value = data.get('relayPH')
+        relayph_value = data.get("relayPH")
         if relayph_value is not None:
             return relayph_value
         else:
             print("Failed to retrieve 'relayPH' value from Firebase.")
             return None
     else:
-        print(f"Failed to retrieve data from Firebase. HTTP Status code: {response.status_code}")
+        print(
+            f"Failed to retrieve data from Firebase. HTTP Status code: {response.status_code}"
+        )
         return None
+
 
 def update_relayph_in_firebase(url, new_relayph):
     """
@@ -34,13 +38,28 @@ def update_relayph_in_firebase(url, new_relayph):
     Returns:
     bool: True if the update was successful, False otherwise.
     """
-    updated_data = {
-        'relayPH': new_relayph
-    }
+    updated_data = {"relayPH": new_relayph}
     response = requests.put(url, json=updated_data)
     if response.status_code == 200:
         print("RelayPH value updated successfully.")
         return True
     else:
-        print(f"Failed to update RelayPH value in Firebase. HTTP Status code: {response.status_code}")
+        print(
+            f"Failed to update RelayPH value in Firebase. HTTP Status code: {response.status_code}"
+        )
         return False
+
+
+def update_firebase_data(url, data):
+    """
+    Update data in Firebase Realtime Database.
+
+    Args:
+    url (str): The URL to perform the PUT request.
+    data (dict): Dictionary containing fields to update.
+
+    Returns:
+    tuple: A tuple containing the JSON response from the Firebase API and the HTTP status code.
+    """
+    response = requests.put(url, json=data)
+    return response.json(), response.status_code
