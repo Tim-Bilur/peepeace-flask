d6ef5f42c486fd55264433f72a394ae8375d1ba8
Vinzzztty <kevinarnandes21@gmail.com>
2024-07-08 23:59:30 +0700

    minor update

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
-# This Flask APP Server
-
 from flask import Flask, jsonify, request
 import requests
-from utils.firebase2 import update_firebase_data, update_relayph_in_firebase
-from utils.firebase2 import update_data, update_relay, store_data
 from utils.config import get_firebase
 
 # Define the URL for the GET request
 url = get_firebase()
 
 url_index = f"{url}.json"
-url_update_data = f"{url}/data"
-url_update_relay = f"{url}/relay"
+url_update_data = f"{url}/data.json"
+url_update_relay = f"{url}/relay.json"
 
 app = Flask(__name__)
 
@@ -25,54 +21,32 @@ def index():
 def firebase_response():
     response = requests.get(url_index)
     data = response.json()
+    print(data)
     return jsonify(data)
 
 
-@app.route("/firebase", methods=["PUT"])
+@app.route("/data", methods=["GET"])
+def firebase_data():
+    response = requests.get(url_update_data)
+    data = response.json()
+    print(data)
+    return jsonify(data)
+
+
+@app.route("/data", methods=["PUT"])
 def update_data():
-    data = request.json
-    if data:
-        response, status_code = update_firebase_data(url, data)  # Fix the function call
-        return jsonify(response), status_code
-    else:
-        return jsonify({"error": "No data provided for update."}), 400
+    response = requests.get(url_update_data)
+    data = response.json()
+    print(data)
+    return jsonify(data)
 
 
-@app.route("/ph", methods=["PUT"])
-def update_relay():
-    data = request.json
-    if data and "relay" in data:
-        relay = data["relay"]
-        response_status = update_relayph_in_firebase(url, relay)
-        if response_status:
-            return jsonify({"message": "Relay value updated successfully."}), 200
-        else:
-            return jsonify({"error": "Failed to update Relay value in Firebase."}), 500
-    else:
-        return jsonify({"error": "Invalid data format or 'relay' field missing."}), 400
-
-
-@app.route("/update_data", methods=["PUT"])
-def update_data_route():
-    data = request.json
-    if data:
-        response, status_code = update_firebase_data(url_update_data, data)
-        return jsonify(response), status_code
-    else:
-        return jsonify({"error": "No data provided for update."}), 400
-
-
-@app.route("/update_relay", methods=["PUT"])
-def update_relay_route():
-    data = request.json
-    if data and "relay" in data:
-        response_status = store_data(url_update_relay, data)
-        if response_status:
-            return jsonify({"message": "Relay value updated successfully."}), 200
-        else:
-            return jsonify({"error": "Failed to update relay value in Firebase."}), 500
-    else:
-        return jsonify({"error": "Invalid data format or 'relay' field missing."}), 400
+@app.route("/relay", methods=["GET"])
+def firebase_relay():
+    response = requests.get(url_update_relay)
+    data = response.json()
+    print(data)
+    return jsonify(data)
 
 
 if __name__ == "__main__":
